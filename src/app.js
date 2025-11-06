7aa7a27401dd411d5108ca2f57ac9642a0335504
jinayem <jinayem4@gmail.com>
2025-11-07 00:11:03 +0600

    Implement ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express(); // Creating an instance of an Express application
 
 // Endpoint to handle GET requests to the root URL
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from Acquisitions!');
-})
+  res.status(200).send('Hello from Acquisitions!');
+});
 
 export default app; // Exporting the Express application instance
--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 3000; // Defining the port number
 
 // Starting the server and Listening on the specified port
 app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`); // Logging a message to indicate the server is running
+  console.log(`Listening on http://localhost:${PORT}`); // Logging a message to indicate the server is running
 });
